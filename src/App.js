ef98777cc4d10503f024de01d3ae3904c003d536
raul302 <luis_1547@hotmail.com>
2020-08-26 17:14:58 -0500

    FirstComponent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Componentes
+function FirstComponent(){
+  // JSX return view
+  return(
+    <div id="first">Primer componente</div>
+  )
 }
+
+// const App = () => <div>Este es mi : <FirstComponent></FirstComponent></div>
+
+class App extends React.Component {
+  render(){
+    return <div>Este es mi :  <FirstComponent/></div>
+  }
+}
+// Componentes
+// function App() {
+//   return (
+//     // JSX
+//     <div>Este es mi : <FirstComponent></FirstComponent></div>
+//   );
+// }
 
 export default App;
