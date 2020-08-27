7aff752e7fa4fb40451604dae6b26c52f3138330
raul302 <luis_1547@hotmail.com>
2020-08-27 18:51:19 -0500

    props && state

--- src/App.js
+++ src/App.js
@@ -3,26 +3,66 @@ import React from 'react';
 import './App.css';
 
 // Componentes
-function FirstComponent(){
+function FirstComponent(props){
+  // Three calls
+  console.log(props);
   // JSX return view
   return(
-    <div id="first">Primer componente</div>
+    <div id="first">
+      <h3>{props.title}</h3>
+      {props.text}
+      </div>
   )
+}
+
+class SecondComponent extends React.Component{
+  
+  state={
+    show:true
+  }
+  changeState(){
+      this.setState({show:  !this.state.show})
+  }
+  render(){
+    if(this.state.show){
+      return (
+        <div id="second">
+          <h1>{this.props.title}</h1>
+          <p>{this.props.text}</p>
+          {/* /* <button onClick={()=>alert('change!')}>Change state</button> */} 
+          {/* <button onClick={()=> this.state.show = false}>Change state</button> */}
+          <button onClick={()=> this.changeState()}>Change state</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Show is false</h1>
+          <button onClick={()=> this.changeState()}>Change state</button>
+        </div>
+      )
+    }
+  }
 }
 
 // const App = () => <div>Este es mi : <FirstComponent></FirstComponent></div>
 
-class App extends React.Component {
-  render(){
-    return <div>Este es mi :  <FirstComponent/></div>
-  }
-}
-// Componentes
-// function App() {
-//   return (
-//     // JSX
-//     <div>Este es mi : <FirstComponent></FirstComponent></div>
-//   );
+// class App extends React.Component {
+//   render(){
+//     return <div>Este es mi :  <FirstComponent/></div>
+//   }
 // }
+// Componentes
+ function App() {
+   return (
+      //JSX
+     <div>Este es mi : 
+       <FirstComponent title="this is my title" text="first Hello world"/> 
+       <FirstComponent text="second hello world"/> 
+       <FirstComponent text="last hello world"/>
+       <SecondComponent title="props && state" text="show my state"/>
+     </div>
+   );
+ }
 
 export default App;
