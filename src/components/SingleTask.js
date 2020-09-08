45b354bfa56ccc5760fe33663d92e9f7d674843a
raul302 <luis_1547@hotmail.com>
2020-09-08 17:26:57 -0500

    Finally,Styles with Objects

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -1,20 +1,42 @@
 import React,{Component} from 'react';
+import './SingleTask.css';
 
 class SingleTask extends Component {
+
+    StyleCompleted(){
+        return {
+            fontSize:'20px',
+            color: this.props.task.done ? 'pink' : 'black',
+            textDecoration:'none',
+        }
+    }
     render(){
         // extraction
         const {task} = this.props;
-        
-        return  <div>
+        const redColor = {background : 'red'};
+        return  <div
+        style={this.StyleCompleted()}
+        // style={redColor}
+        >
         {task.title} -
         {task.description} -
         {task.done} -
         {task.id}
         <input type="checkbox"/>
-        <button>
+        <button style={btnDelete}>
             x
         </button>
         </div>
     }
 }
+const btnDelete = {
+    fontSize: '32px',
+    background: 'blue',
+    color: 'orange',
+    border: 'none',
+    padding: '10px 15px',
+    cursor : 'pointer',
+    borderRadius: '50%',
+    float: 'right'
+};
 export default SingleTask;
